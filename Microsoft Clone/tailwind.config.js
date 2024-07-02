8c18db5b8b3ec110f573207b3ebb69678aecd0b3
Akib-7 <mu.aqib7@gmail.com>
2024-07-02 20:32:20 +0500

    Initial commit

---
+++ Microsoft Clone/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+        background:"#c2def9"
+      }
+    },
+  },
+  plugins: [],
+}
+
